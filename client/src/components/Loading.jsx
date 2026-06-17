e4d3aaef355a0352a9329141176aebb1ab880d29
Ankitnandanwar <ankitnandanwar3@gmail.com>
2026-06-17 10:56:38 +0530

    Dashboard Page UI Completed

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin size-8 border-2 border-indigo-500 border-t-transparent rounded-full"/>
+    </div>
+  )
+}
+
+export default Loading
